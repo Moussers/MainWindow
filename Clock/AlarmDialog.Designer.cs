9cde69df943793f423ed3249e4f7815834d634bc
sandCat <Sand@DESKTOP-K3SU1F7>
2026-01-14 22:28:21 +0300

    Cancel button DONE

--- Clock/AlarmDialog.Designer.cs
+++ Clock/AlarmDialog.Designer.cs
@@ -140,6 +140,7 @@
             this.buttonCancel.TabIndex = 8;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // AlarmDialog
             // 
