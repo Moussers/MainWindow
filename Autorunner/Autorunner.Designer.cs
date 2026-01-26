d1dd1d6a16026a2050082ede10b498c72cac687c
sandCat <Sand@DESKTOP-K3SU1F7>
2026-01-26 22:33:17 +0300

    Autorun in process

---
+++ Autorunner/Autorunner.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Autorunner
+{
+    partial class Autorunner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelnfo = new System.Windows.Forms.Label();
+            this.checkBoxLaunch = new System.Windows.Forms.CheckBox();
+            this.buttonComplete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelnfo
+            // 
+            this.labelnfo.AutoSize = true;
+            this.labelnfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelnfo.Location = new System.Drawing.Point(22, 35);
+            this.labelnfo.Name = "labelnfo";
+            this.labelnfo.Size = new System.Drawing.Size(445, 25);
+            this.labelnfo.TabIndex = 0;
+            this.labelnfo.Text = "Поздравляем, Вы устанвили ClockPV_521! ";
+            // 
+            // checkBoxLaunch
+            // 
+            this.checkBoxLaunch.AutoSize = true;
+            this.checkBoxLaunch.Location = new System.Drawing.Point(27, 79);
+            this.checkBoxLaunch.Name = "checkBoxLaunch";
+            this.checkBoxLaunch.Size = new System.Drawing.Size(220, 17);
+            this.checkBoxLaunch.TabIndex = 1;
+            this.checkBoxLaunch.Text = "Запустить установленную программу!";
+            this.checkBoxLaunch.UseVisualStyleBackColor = true;
+            // 
+            // buttonComplete
+            // 
+            this.buttonComplete.Location = new System.Drawing.Point(384, 129);
+            this.buttonComplete.Name = "buttonComplete";
+            this.buttonComplete.Size = new System.Drawing.Size(83, 23);
+            this.buttonComplete.TabIndex = 2;
+            this.buttonComplete.Text = "Заавершить";
+            this.buttonComplete.UseVisualStyleBackColor = true;
+            this.buttonComplete.Click += new System.EventHandler(this.buttonComplete_Click);
+            // 
+            // Autorunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 169);
+            this.Controls.Add(this.buttonComplete);
+            this.Controls.Add(this.checkBoxLaunch);
+            this.Controls.Add(this.labelnfo);
+            this.Name = "Autorunner";
+            this.Text = "Заврешение установки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelnfo;
+        private System.Windows.Forms.CheckBox checkBoxLaunch;
+        private System.Windows.Forms.Button buttonComplete;
+    }
+}
+
---
+++ Autorunner/Autorunner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Autorunner
+{
+    public partial class Autorunner : Form
+    {
+        public Autorunner()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonComplete_Click(object sender, EventArgs e)
+        {
+            if (checkBoxLaunch.Checked)
+            {
+                Process.Start("Clock.exe");
+            }
+        }
+    }
+}
